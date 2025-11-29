24d3a058f6b3a8b5f17f4672e6d0ca9ea5d3e2b0
eshnd <eshaan2031@icloud.com>
2025-11-29 00:24:23 -0600

    algorithms and ranges practice and also restructuring to allow lsp to use c++23

---
+++ src/algorithms_practice.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <ranges>
+#include <vector>
+
+int main() {
+  std::vector<int> num_list;
+
+  while (true) {
+    bool valid_input_given = false;
+    std::string int_str;
+    while (!valid_input_given) {
+      std::cout << "Enter a number: ";
+      std::getline(std::cin, int_str);
+      if (int_str == "done") {
+        if (num_list.size() == 0) {
+          std::cout << "No values provided" << '\n';
+          return 0;
+        }
+
+        std::cout << "Length: " << num_list.size() << '\n';
+        std::cout << "Sum: "
+                  << std::accumulate(num_list.begin(), num_list.end(), 0)
+                  << '\n';
+        std::cout << "Min: "
+                  << *std::min_element(num_list.begin(), num_list.end())
+                  << '\n';
+        std::cout << "Max: "
+                  << *std::max_element(num_list.begin(), num_list.end())
+                  << '\n';
+        auto transformed_num_list =
+            num_list | std::views::transform([](int n) { return n + 1; });
+        std::cout << '[';
+        std::ranges::for_each(transformed_num_list,
+                              [](int n) { std::cout << n << ','; });
+        std::cout << "]\n";
+        return 0;
+      }
+
+      try {
+        num_list.push_back(std::stoi(int_str));
+        valid_input_given = true;
+      } catch (const std::exception &e) {
+        std::cout << "Error: " << e.what() << '\n';
+      }
+    }
+  }
+}
